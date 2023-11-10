cdafc9bc2dbb8a9442f7fa1645aaf217b19cbd54
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-11-10 20:28:38 +0100

    feat: bdd

--- pages/auth/login/index.js
+++ pages/auth/login/index.js
@@ -2,7 +2,7 @@ import ContainerBlock from "@/components/ContainerBlock";
 import { ColumnWithImage } from "@/components/login/ColumnWithImage";
 import { Box, Flex } from "@chakra-ui/react";
 import Footer from "@/components/Footer";
-import FormLogin from "@/components/login/FormLogin";
+
 
 
 export default function LoginPage() {
