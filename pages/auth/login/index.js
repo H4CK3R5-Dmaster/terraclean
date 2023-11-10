67b9eb86a1812c6d0a7f810d6c87e22d605d739a
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-11-10 12:20:23 +0100

    feat: bdd

---
+++ pages/auth/login/index.js
@@ -0,0 +1,10 @@
+import ContainerBlock from "@/components/ContainerBlock";
+
+
+export default function LoginPage() {
+    return (
+        <ContainerBlock>
+            
+        </ContainerBlock>
+    )
+}
