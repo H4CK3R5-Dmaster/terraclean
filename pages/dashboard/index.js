25ffa3afbad0fab8f79ec9771637c75c82bc503e
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-12-07 22:02:19 +0100

    feat: qrcode done

---
+++ pages/dashboard/index.js
@@ -0,0 +1,9 @@
+import ContainerBlock from "@/components/ContainerBlock";
+
+export default function Dashboard() {
+    return (
+        <ContainerBlock>
+            
+        </ContainerBlock>
+    )
+}
